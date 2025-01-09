473b8d4dd8c16fbe772d8ecffb48cd7bab94c7ee
MeyAdam <meyadam11@gmail.com>
2025-01-09 19:38:42 +0800

    section Gender UI first draft

--- src/components/Gender/Gender.jsx
+++ src/components/Gender/Gender.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import css from './Gender.module.css'
+import React from "react";
+import imgMen from "../../assets/imgMen.png";
+import imgWomen from "../../assets/imgWomen.png";
+import WrapperWhite from "../WrapperWhite/WrapperWhite";
+import css from "./Gender.module.css";
 
 const Gender = () => {
   return (
-    <div>Gender</div>
-  )
-}
+    <section className={css.sectionGender}>
+      <WrapperWhite className={css.wrapperGender}>
+        <h2>What's your gender?</h2>
+        <div className={css.wrapperGenderChoice}>
+          <div className={css.gender}>
+            <img src={imgMen} alt="Men" />
+            <p>Men</p>
+          </div>
+          <div className={css.gender}>
+            <img src={imgWomen} alt="Women" />
+            <p>Women</p>
+          </div>
+        </div>
+      </WrapperWhite>
+    </section>
+  );
+};
 
-export default Gender
+export default Gender;
