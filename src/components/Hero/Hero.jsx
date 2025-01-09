a4fab971cb338185453821fa6fecaf4423f43dc4
MeyAdam <meyadam11@gmail.com>
2025-01-09 18:00:37 +0800

    section Hero UI first draft

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,13 @@ import css from './Hero.module.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={css.sectionHero} style={{backgroundColor: 'green'}}>
+      <div className={css.wrapperHero}>
+        <h1>Calculate how much you need to retire</h1>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</h2>
+        <button type='button'>Start Now</button>
+      </div>
+    </section>
   )
 }
 
