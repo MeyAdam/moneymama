6fff1d42649a6093ae53bbb5f0d9b714d7ae8ec8
MeyAdam <meyadam11@gmail.com>
2025-01-09 23:28:59 +0800

    component BtnYellow

---
+++ src/components/BtnYellow/BtnYellow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BtnYellow = () => {
+  return (
+    <a href="/">
+      <button type="button" className="btnYellow">
+        BtnYellow
+      </button>
+    </a>
+  );
+};
+
+export default BtnYellow;
