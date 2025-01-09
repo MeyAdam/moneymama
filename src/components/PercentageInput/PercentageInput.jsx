7ffd06e114b17f7f4640a8e9677d9df89f9c1ef0
MeyAdam <meyadam11@gmail.com>
2025-01-10 00:43:52 +0800

    section percentage input UI first draft

--- src/components/PercentageInput/PercentageInput.jsx
+++ src/components/PercentageInput/PercentageInput.jsx
@@ -1,10 +1,120 @@
-import React from 'react'
-import css from './PercentageInput.module.css'
+import React, { useState } from "react";
+import WrapperWhite from "../WrapperWhite/WrapperWhite";
+import css from "./PercentageInput.module.css";
 
 const PercentageInput = () => {
-  return (
-    <div>PercentageInput</div>
-  )
-}
+  const [savingsRate, setSavingsRate] = useState(85.38);
+  const [preInvestmentReturn, setPreInvestmentReturn] = useState(50);
+  const [postInvestmentReturn, setPostInvestmentReturn] = useState(17.56);
 
-export default PercentageInput
+  const handleSavingsRateChange = (e) => {
+    setSavingsRate(e.target.value);
+  };
+
+  const handlePreInvestmentReturnChange = (e) => {
+    setPreInvestmentReturn(e.target.value);
+  };
+
+  const handlePostInvestmentReturnChange = (e) => {
+    setPostInvestmentReturn(e.target.value);
+  };
+
+  return (
+    <section className={css.sectionPercentageInput}>
+      <WrapperWhite className={css.wrapperPercentageInput}>
+        <div className={css.wrapperPercentage}>
+          <p>Savings rate</p>
+          <div className={css.wrapperInputValue}>
+            <input
+              className={css.rangeInput}
+              type="range"
+              name="savingsRate"
+              id=""
+              min="0"
+              max="100"
+              step="0.01"
+              value={savingsRate}
+              onChange={handleSavingsRateChange}
+              style={{
+                background: `linear-gradient(to right, var(--yellow) ${savingsRate}%, #F7F8FF ${savingsRate}%)`,
+              }}
+            />
+            <input
+              className={css.numberInput}
+              type="number"
+              name="savingsRate"
+              id=""
+              min="0"
+              max="100"
+              step="0.01"
+              value={savingsRate}
+              onChange={handleSavingsRateChange}
+            />
+          </div>
+        </div>
+        <div className={css.wrapperPercentage}>
+          <p>Pre-retirement investment return %</p>
+          <div className={css.wrapperInputValue}>
+            <input
+              className={css.rangeInput}
+              type="range"
+              name="preInvestmentReturn"
+              id=""
+              min="0"
+              max="100"
+              step="0.01"
+              value={preInvestmentReturn}
+              onChange={handlePreInvestmentReturnChange}
+              style={{
+                background: `linear-gradient(to right, var(--yellow) ${preInvestmentReturn}%, #F7F8FF ${preInvestmentReturn}%)`,
+              }}
+            />
+            <input
+              className={css.numberInput}
+              type="number"
+              name="preInvestmentReturn"
+              id=""
+              min="0"
+              max="100"
+              step="0.01"
+              value={preInvestmentReturn}
+              onChange={handlePreInvestmentReturnChange}
+            />
+          </div>
+        </div>
+        <div className={css.wrapperPercentage}>
+          <p>Post-retirement Investment return %</p>
+          <div className={css.wrapperInputValue}>
+            <input
+              className={css.rangeInput}
+              type="range"
+              name="postInvestmentReturn"
+              id=""
+              min="0"
+              max="100"
+              step="0.01"
+              value={postInvestmentReturn}
+              onChange={handlePostInvestmentReturnChange}
+              style={{
+                background: `linear-gradient(to right, var(--yellow) ${postInvestmentReturn}%, #F7F8FF ${postInvestmentReturn}%)`,
+              }}
+            />
+            <input
+              className={css.numberInput}
+              type="number"
+              name="postInvestmentReturn"
+              id=""
+              min="0"
+              max="100"
+              step="0.01"
+              value={postInvestmentReturn}
+              onChange={handlePostInvestmentReturnChange}
+            />
+          </div>
+        </div>
+      </WrapperWhite>
+    </section>
+  );
+};
+
+export default PercentageInput;
