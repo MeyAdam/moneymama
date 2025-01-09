c51a440c61a083282de73cb87b1303cdf3caf4d0
MeyAdam <meyadam11@gmail.com>
2025-01-09 19:37:59 +0800

    section PlanSteps first draft

--- src/components/PlanSteps/PlanSteps.jsx
+++ src/components/PlanSteps/PlanSteps.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import css from './PlanSteps.module.css'
+import React from "react";
+import css from "./PlanSteps.module.css";
 
 const PlanSteps = () => {
   return (
-    <div>PlanSteps</div>
-  )
-}
+    <section className={css.sectionPlanSteps}>
+      <div className={css.wrapperPlanSteps}>
+        <div className={css.heading}>
+          <h2>Plan Your Retirement Savings Today</h2>
+          <p>
+            This tool will help you estimate how much you need to retire just in
+            2 minutes!
+          </p>
+        </div>
+        <div>steps</div>
+      </div>
+    </section>
+  );
+};
 
-export default PlanSteps
+export default PlanSteps;
