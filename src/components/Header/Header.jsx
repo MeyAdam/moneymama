b13acfad303d1f0c03415f80f39639b798cbe9e0
MeyAdam <meyadam11@gmail.com>
2025-01-09 17:53:00 +0800

    UI header first draft

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import css from './Header.module.css'
+import React from "react";
+import logo from "../../assets/MONEYMAMA-logo.svg";
+import css from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={css.header}>
+      <div className={css.wrapper}>
+        <img src={logo} alt="logo" className={css.logo} />
+        <nav>
+          <ul className={css.navList}>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
