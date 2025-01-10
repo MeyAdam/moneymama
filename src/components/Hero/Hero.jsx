2a7d056f776360e030a43ae191bfd8e6a202ef24
MeyAdam <meyadam11@gmail.com>
2025-01-11 02:30:44 +0800

    section Hero UI update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import css from './Hero.module.css'
+import React from "react";
+import BtnYellow from "../BtnYellow/BtnYellow";
+import css from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <section className={css.sectionHero} style={{backgroundColor: 'green'}}>
+    <section className={css.sectionHero}>
       <div className={css.wrapperHero}>
-        <h1>Calculate how much you need to retire</h1>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</h2>
-        <button type='button'>Start Now</button>
+        <h1>
+          Calculate how much you need to{" "}
+          <span style={{ color: "var(--salmonRed)", fontStyle: "italic" }}>
+            retire
+          </span>
+        </h1>
+        <p className={css.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </p>
+        <BtnYellow text="Start Now" />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
