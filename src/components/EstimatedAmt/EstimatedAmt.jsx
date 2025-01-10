84a7e2ef1ec181acfde1c243c1692b0795bb249c
MeyAdam <meyadam11@gmail.com>
2025-01-11 01:36:04 +0800

    section estimated amount UI first draft

--- src/components/EstimatedAmt/EstimatedAmt.jsx
+++ src/components/EstimatedAmt/EstimatedAmt.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import css from './EstimatedAmt.module.css'
+import React from "react";
+import css from "./EstimatedAmt.module.css";
+import WrapperWhite from "../WrapperWhite/WrapperWhite";
 
 const EstimatedAmt = () => {
   return (
-    <div>EstimatedAmt</div>
-  )
-}
+    <section className={css.sectionEstimatedAmt}>
+      <WrapperWhite className={css.wrapperEstimatedAmt}>
+        <p style={{ color: "var(--grey01)" }}>
+          Estimated amount you need to retire
+        </p>
+        <p className={css.amount}>RM1,600,000</p>
+      </WrapperWhite>
+    </section>
+  );
+};
 
-export default EstimatedAmt
+export default EstimatedAmt;
