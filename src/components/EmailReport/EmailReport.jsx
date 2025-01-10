e7990e6fb4baba78b1be1514aaeb1306d4dc74da
MeyAdam <meyadam11@gmail.com>
2025-01-11 01:35:39 +0800

    section email report UI first draft

--- src/components/EmailReport/EmailReport.jsx
+++ src/components/EmailReport/EmailReport.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import css from './EmailReport.module.css'
+import React from "react";
+import BtnYellow from "../BtnYellow/BtnYellow";
+import css from "./EmailReport.module.css";
 
 const EmailReport = () => {
   return (
-    <div>EmailReport</div>
-  )
-}
+    <section className={css.sectionEmailReport}>
+      <div className={css.wrapperEmailReport}>
+        <h2>Get your report direct to your inbox</h2>
+        <div className={css.wrapperEmail}>
+          <input type="email" placeholder="Enter email" className={css.inputEmail} />
+          <BtnYellow text="Get my report" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default EmailReport
+export default EmailReport;
