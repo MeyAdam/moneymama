79711aefe87b33d11d2b60fb94cf1065c2c779d0
MeyAdam <meyadam11@gmail.com>
2025-01-11 15:26:40 +0800

    update section Plan Steps responsiveness

--- src/components/PlanSteps/PlanSteps.jsx
+++ src/components/PlanSteps/PlanSteps.jsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import React from "react";
+import { FaCheck } from "react-icons/fa";
 import css from "./PlanSteps.module.css";
 
 const PlanSteps = () => {
@@ -7,12 +9,25 @@ const PlanSteps = () => {
       <div className={css.wrapperPlanSteps}>
         <div className={css.heading}>
           <h2>Plan Your Retirement Savings Today</h2>
-          <p>
+          <p className={css.desc}>
             This tool will help you estimate how much you need to retire just in
             2 minutes!
           </p>
         </div>
-        <div>steps</div>
+        <div className={css.wrapperSteps}>
+          <div className={classNames(css.step, css.active)}>
+            <p>1</p>
+          </div>
+          <div className={css.step}>
+            <p>2</p>
+          </div>
+          <div className={css.step}>
+            <p>3</p>
+          </div>
+          <div className={css.step}>
+            <FaCheck />
+          </div>
+        </div>
       </div>
     </section>
   );
