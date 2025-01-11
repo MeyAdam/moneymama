bd5f6fba730f937364b2e679af3badee34f3c340
MeyAdam <meyadam11@gmail.com>
2025-01-11 20:34:04 +0800

    update section Estimated Amt UI responsiveness

--- src/components/EstimatedAmt/EstimatedAmt.jsx
+++ src/components/EstimatedAmt/EstimatedAmt.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import css from "./EstimatedAmt.module.css";
 import WrapperWhite from "../WrapperWhite/WrapperWhite";
+import css from "./EstimatedAmt.module.css";
 
 const EstimatedAmt = () => {
   return (
     <section className={css.sectionEstimatedAmt}>
       <WrapperWhite className={css.wrapperEstimatedAmt}>
-        <p style={{ color: "var(--grey01)" }}>
-          Estimated amount you need to retire
-        </p>
+        <p className={css.title}>Estimated amount you need to retire</p>
         <p className={css.amount}>RM1,600,000</p>
       </WrapperWhite>
     </section>
