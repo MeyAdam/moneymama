0cbc4fe111e4308d49eb34eecb389dcdc832b1d4
MeyAdam <meyadam11@gmail.com>
2025-01-11 20:24:25 +0800

    update section total accumulated UI responsiveness

--- src/components/TotalAccumulated/TotalAccumulated.jsx
+++ src/components/TotalAccumulated/TotalAccumulated.jsx
@@ -14,33 +14,26 @@ const TotalAccumulated = () => {
         <div className={css.boxEstimatedDetails}>
           {/* age is dynamic from retiredAge input */}
           <p className={css.title}>Estimated savings by age 55</p>
-          <p>RM990,700</p>
+          <p className={css.amount}>RM990,700</p>
         </div>
-
-        <div class={css.verticalLine}></div>
 
         <div className={css.boxEstimatedDetails}>
           <p className={css.title}>Currently you're short of</p>
-          <p>RM250,300</p>
+          <p className={css.amount}>RM250,300</p>
         </div>
 
         <div className={css.boxEstimatedDetails}>
           <p className={css.title}>You have to save (monthly)</p>
-          <p>RM1,200</p>
+          <p className={css.amount}>RM1,200</p>
         </div>
-
-        <div class={css.verticalLine}></div>
         
         <div className={css.boxEstimatedDetails}>
           <p className={css.title}>Investment growth rate</p>
-          <p style={{color: "var(--grey02)"}}>
+          <p style={{color: "var(--grey02)", fontSize: "16px"}}>
             You'll need to save <span style={{color: "var(--black)"}}>RM1,200</span> monthly at an investment growth rate of <span style={{color: "var(--black)"}}>{investmentGrowthRate}%</span> to achieve your goal
           </p>
           <div className={css.wrapperInputGroup}>
-            <span className={css.inputGroupText}>%</span>
-            <span style={{ padding: "0 10px", color: "var(--lightGrey)" }}>
-              |
-            </span>
+            <p className={css.inputGroupText}>%</p>
             <input
               className={css.investmentGrowthRate}
               type="number"
