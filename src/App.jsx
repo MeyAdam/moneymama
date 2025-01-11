163969b30518596682543856ad6aaf26f7861903
MeyAdam <meyadam11@gmail.com>
2025-01-11 21:33:26 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AgeInput from "./components/AgeInput/AgeInput";
+import BtnYellow from "./components/BtnYellow/BtnYellow";
 import EmailReport from "./components/EmailReport/EmailReport";
 import EstimatedAmt from "./components/EstimatedAmt/EstimatedAmt";
 import Footer from "./components/Footer/Footer";
@@ -18,12 +19,52 @@ const App = () => {
       <Header />
       <Hero />
       <PlanSteps />
-      <Gender />
-      <AgeInput />
-      <MoneyInput />
-      <PercentageInput />
-      <EstimatedAmt />
-      <TotalAccumulated />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "80px 0",
+          gap: "80px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "80px",
+          }}
+        >
+          <Gender />
+          <AgeInput />
+          <BtnYellow text="Next" />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "80px",
+          }}
+        >
+          <MoneyInput />
+          <PercentageInput />
+          <BtnYellow text="See Result" />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "80px",
+          }}
+        >
+          <EstimatedAmt />
+          <TotalAccumulated />
+          <BtnYellow text="Recalculate" />
+        </div>
+      </div>
       <TotalAccumulatedDetails />
       <EmailReport />
       <Footer />
