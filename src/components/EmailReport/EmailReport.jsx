f796090d5a1db62d9f0db0579ba7aabd948c08e7
MeyAdam <meyadam11@gmail.com>
2025-01-21 02:21:12 +0800

    simulating sent email

--- src/components/EmailReport/EmailReport.jsx
+++ src/components/EmailReport/EmailReport.jsx
@@ -1,21 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import BtnYellow from "../BtnYellow/BtnYellow";
+import { FaCircleCheck } from "react-icons/fa6";
 import css from "./EmailReport.module.css";
 
 const EmailReport = () => {
+  const [email, setEmail] = useState("");
+  const [isSent, setIsSent] = useState(false);
+  const [isSending, setIsSending] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleEmailSubmit = async () => {
+    if (!email) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
+    setIsSending(true); // loading state
+
+    try {
+      // Simulate sending the email
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating network delay
+
+      setIsSent(true); // Update the button to "Sent"
+    } catch (error) {
+      alert("Failed to send the email. Please try again.");
+    } finally {
+      setIsSending(false); // Reset loading state
+    }
+  };
+
   return (
     <section className={css.sectionEmailReport}>
       <div className={css.wrapperEmailReport}>
         <h2 style={{ textAlign: "center", fontSize: "28px" }}>
-          Get your report direct to your inbox
+          Would you like an email copy of your report?
         </h2>
         <div className={css.wrapperEmail}>
           <input
             type="email"
             placeholder="Enter email"
             className={css.inputEmail}
+            value={email}
+            onChange={handleEmailChange}
           />
-          <BtnYellow text="Get my report" className={css.btnYellowAdd} />
+          {!isSent ? (
+            <button
+              className="btnYellow"
+              type="button"
+              onClick={handleEmailSubmit}
+              disabled={isSending}
+            >
+              {isSending ? "Sending..." : "Get my report"}
+            </button>
+          ) : (
+            <button
+              className="btnYellow"
+              type="button"
+              disabled
+              style={{ display: "flex", alignItems: "center", gap: "5px", justifyContent: "center" }}
+            >
+              <FaCircleCheck color="#34C759" />
+              Sent!
+            </button>
+          )}
         </div>
       </div>
     </section>
